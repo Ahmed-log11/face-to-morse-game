35bd63d44d5280cc31bebbb7bd6b31358edfb73b
medoveiol <rahmaobaid17@gmail.com>
2026-04-18 20:46:50 +0300

    rename Welcome to Screen1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Welcome from './pages/Welcome'; 
+import Welcome from './pages/Screen1'; 
 import Calibration from './pages/screen_3';
 import GameScreen from './pages/GameScreen';
 
@@ -9,7 +9,7 @@ function App() {
       <div className="bg-black min-h-screen">
         <Routes>
           {/* Default Route: localhost:5173/ */}
-          <Route path="/" element={<Welcome />} />
+          <Route path="/" element={<Screen1 />} />
           
           {/* Screen 3 Route: localhost:5173/3 */}
           <Route path="/3" element={<Calibration />} />
